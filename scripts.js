590d77c9c05f9c44345109edc3559af6c7719e6a
Alberto Dzay <dzay.programacion@gmail.com>
2022-06-09 12:01:51 -0500

    Adicion del calculo del vertice, ordenada y evaluacion de la funcion cuadratica

--- scripts.js
+++ scripts.js
@@ -1,13 +1,18 @@
 const inputA = document.querySelector("#coef-A");
 const inputB = document.querySelector("#coef-B");
 const inputC = document.querySelector("#coef-C");
+const inputX = document.querySelector("#eval-x");
 const btn = document.querySelector("#btn");
 const label_x1 = document.querySelector("#x1");
 const label_x2 = document.querySelector("#x2");
+const label_vert = document.querySelector("#vertice");
+const label_minmax = document.querySelector("#minmax");
+const label_ord = document.querySelector("#ordenada");
+const label_eval = document.querySelector("#eval");
 
 function fgeneral(a,b,c) {
     const valor_interno_raiz = b*b-4*a*c;
-    if (valor_interno_raiz > 0) {
+    if (valor_interno_raiz >= 0) {
         const raiz = Math.sqrt(valor_interno_raiz);
         const x1 = (-b+raiz)/(2*a);
         const x2 = (-b-raiz)/(2*a);
@@ -16,19 +21,43 @@ function fgeneral(a,b,c) {
     return "No tiene solucion real";
 }
 
-btn.onclick = function() {
-    const a = inputA.value;
-    const b = inputB.value;
-    const c = inputC.value;
+function f(x) {
+    const a = parseFloat(inputA.value);
+    const b = parseFloat(inputB.value);
+    const c = parseFloat(inputC.value);
+    const fx = a*x*x + b*x + c;
+    return fx;
+}
 
-    const resultado = fgeneral(a,b,c);
-    if (typeof(resultado) == "object") {
-        const [x1,x2] = resultado;
+function hallar_vertice(a,b) {
+    const x = -b/(2*a);
+    let minmax = "";
+    if (a > 0) {minmax = "mínimo"}
+    else {minmax = "máximo"}
+    return [x,f(x),minmax];
+}
+
+btn.onclick = function() {
+    const a = parseFloat(inputA.value);
+    const b = parseFloat(inputB.value);
+    const c = parseFloat(inputC.value);
+    const x = parseFloat(inputX.value);
+
+    const raices = fgeneral(a,b,c);
+    const [xv,yv,minmax] = hallar_vertice(a,b);
+    const ordenada = f(0);
+    const fx = f(x);
+    if (typeof(raices) == "object") {
+        const [x1,x2] = raices;
         label_x1.innerHTML = `${x1}`;
         label_x2.innerHTML = `${x2}`;
     }
-    if (typeof(resultado) == "string") {
-        label_x1.innerHTML = resultado;
-        label_x2.innerHTML = resultado;
+    if (typeof(raices) == "string") {
+        label_x1.innerHTML = raices;
+        label_x2.innerHTML = raices;
     }
+    label_vert.innerHTML = `(${xv},${yv})`;
+    label_minmax.innerHTML = `${minmax}`;
+    label_ord.innerHTML = `${ordenada}`;
+    label_eval.innerHTML = `${fx}`;
 };
